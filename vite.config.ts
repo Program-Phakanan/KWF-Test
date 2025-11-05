1bee079e583b55ffbe93d857598490763b406d4d
Phakanan Aiemsaard <program.phakanan@gmail.com>
2025-11-05 10:54:17 +0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/KWF-Meeting/',
+  base: '/KWF-Test/',
   server: {
     port: 3000,  // เปลี่ยนเป็น port ที่ต้องการ
     host: true   // เพิ่มถ้าต้องการเข้าถึงจากเครื่องอื่น
